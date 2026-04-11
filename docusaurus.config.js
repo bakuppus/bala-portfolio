26cd2d4f0cb85d681b8a18acd72b886d3b4cdda0
bakuppus <bakuppus@gmail.com>
2026-04-11 09:33:01 +0530

    Add Architecture

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 const config = {
   title: 'BalaSubramani K',
   tagline: 'Trusted Cloud, Kubernetes & AI Infrastructure Partner',
-  url: 'https://bakuppus.github.io',
-  baseUrl: '/bala-portfolio/',
+  url: 'https://bala.kubelancer.com',
+  baseUrl: '/',
 
   trailingSlash: false,
   onBrokenLinks: 'warn',
